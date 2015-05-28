e4ab675d576073a22e70e6b58af1137fe950b68f
Russ Burdick <grub@extrapolation.net>
2015-05-28 16:07:01 -0400

    reverted header change

--- riptag.pl
+++ riptag.pl
@@ -1,6 +1,24 @@
 #!/usr/bin/perl -w
+#
+# riptag.pl - rip cds to mp3 format using CDDB info
+# Copyright (C) 2003-2004 Russ Burdick, grub@extrapolation.net
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
 
-use lib qw(.);
+use lib qw(/path/to/dir/with/CDDB_get2);
 use CDDB_get2 qw( get_cddb get_discids );
 #use CDDB_get;
 #use Env;
@@ -77,7 +95,7 @@ $lame_path       = "/usr/bin/lame";
 $cdparanoia_path = "/usr/bin/cdparanoia";
 
 # Please don't change this when sending patches.
-$VERSION="0.0.3 (23 Feb 2002)";
+$VERSION="0.0.4 (28 May 2015)";
 
 GetOptions( "version"         => \$version_opt,
             "device=s"        => \$cddev,
